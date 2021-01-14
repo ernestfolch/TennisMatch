a7c894653a8da1442631a86f0f50cee1b0f4e0a9
ernestfolch <ernest.folch@gmail.com>
2021-01-14 16:46:21 +0100

    Added failing GetPlayersNameTest

---
+++ TennisMatchTests/TennisMatchTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisMatchTests
+{
+    [TestClass]
+    public class TennisMatchTests
+    {
+        [TestMethod]
+        public void GetPlayersNameTest()
+        {
+            // arrange
+            var match = new Match("Player1 Name", "Player2 Name");
+
+            // asset
+            Assert.AreEqual("Player1 Name", match.GetPlayer1Name());
+            Assert.AreEqual("Player2 Name", match.GetPlayer2Name());
+        }
+    }
+}
