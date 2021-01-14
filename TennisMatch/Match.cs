74c4d1b1cbfffce2cc722a63eab71c27e52cdb0b
ernestfolch <ernest.folch@gmail.com>
2021-01-14 16:50:53 +0100

    Passed GetPlayersNameTest

---
+++ TennisMatch/Match.cs
@@ -0,0 +1,24 @@
+﻿namespace TennisMatch
+{
+    public class Match
+    {
+        private string Player1Name { get; set; }
+        private string Player2Name { get; set; }
+
+        public Match(string player1Name, string player2Name)
+        {
+            Player1Name = player1Name;
+            Player2Name = player2Name;
+        }
+
+        public string GetPlayer1Name()
+        {
+            return Player1Name;
+        }
+
+        public string GetPlayer2Name()
+        {
+            return Player2Name;
+        }
+    }
+}
