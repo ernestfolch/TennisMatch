1a8d8aef252415d695a5e75e552197da7bb956c0
ernestfolch <ernest.folch@gmail.com>
2021-01-16 18:07:43 +0100

    Added startMatch and AddPoint commands

--- TennisMatch.UI/Model/SessionContext.cs
+++ TennisMatch.UI/Model/SessionContext.cs
@@ -7,8 +7,6 @@ namespace TennisMatch.UI.Model
     {
         private Match Match { get; set; }
         private string _player1Name;
-        private string _player2Name;
-
         public string Player1Name
         {
             get
@@ -21,6 +19,7 @@ namespace TennisMatch.UI.Model
                 OnPropertyChanged("Player1Name");
             }
         }
+        private string _player2Name;
         public string Player2Name
         {
             get
@@ -32,13 +31,54 @@ namespace TennisMatch.UI.Model
                 _player2Name = value;
                 OnPropertyChanged("Player2Name");
             }
-        }       
+        }
+        public string Player1GameScore
+        {
+            get
+            {
+                return (Match != null) ? Match.GetPlayerGameScore(PlayerOrder.player1) : "0";
+            }
+        }
+        public string Player2GameScore
+        {
+            get
+            {
+                return (Match != null) ? Match.GetPlayerGameScore(PlayerOrder.player2) : "0";
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Method to start a tennis match
+        /// </summary>
+        public void StartMatch()
+        {
+            if (!string.IsNullOrEmpty(Player1Name) && !string.IsNullOrEmpty(Player2Name))
+                Match = new Match(Player1Name, Player2Name);
+        }
+
+        /// <summary>
+        /// Method to add a game point to the referred player
+        /// </summary>
+        /// <param name="player">The referred player. The values can be Player1 or Player2</param>
+        public void AddPoint(string player)
+        {
+            if (Match != null)
+            {
+                if (player == "Player1")
+                    Match.AddPlayerPoint(PlayerOrder.player1);
+                else
+                    Match.AddPlayerPoint(PlayerOrder.player2);
+
+                OnPropertyChanged("Player1GameScore");
+                OnPropertyChanged("Player2GameScore");
+            }
         }
     }
 }
--- TennisMatch.UI/ViewModel/RefereePanelViewModel.cs
+++ TennisMatch.UI/ViewModel/RefereePanelViewModel.cs
@@ -1,14 +1,32 @@
-﻿using TennisMatch.UI.Model;
+﻿using System;
+using System.Windows.Input;
+using TennisMatch.UI.Command;
+using TennisMatch.UI.Model;
 
 namespace TennisMatch.UI.ViewModel
 {
     public class RefereePanelViewModel
     {
         public SessionContext SessionContext { get; set; }
+        public ICommand StartMatchCommand { get; set; }
+        public ICommand AddPointCommand { get; set; }
 
         public RefereePanelViewModel(SessionContext sessionContext)
         {
             SessionContext = sessionContext;
+
+            StartMatchCommand = new DelegateCommand(new Action<object>(StartMatch));
+            AddPointCommand = new DelegateCommand(new Action<object>(AddPlayerPoint));
+        }
+
+        private void StartMatch(object obj)
+        {
+            SessionContext.StartMatch();
+        }
+
+        private void AddPlayerPoint(object obj)
+        {
+            SessionContext.AddPoint(obj.ToString());
         }
     }
 }
