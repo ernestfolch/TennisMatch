91ff22f66b1360fd6ea7c4b44fa185c8e1414c55
ernestfolch <ernest.folch@gmail.com>
2021-01-16 18:42:55 +0100

    Added won games inside sets

--- TennisMatch.UI/Model/SessionContext.cs
+++ TennisMatch.UI/Model/SessionContext.cs
@@ -5,7 +5,13 @@ namespace TennisMatch.UI.Model
 {
     public class SessionContext : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Gets or sets the shared tennis match object
+        /// </summary>
+        /// <value>The shared tennis match shared</value>
         private Match Match { get; set; }
+
+        #region Player1 properties
         private string _player1Name;
         public string Player1Name
         {
@@ -19,6 +25,81 @@ namespace TennisMatch.UI.Model
                 OnPropertyChanged("Player1Name");
             }
         }
+        public string Player1GameScore
+        {
+            get
+            {
+                return (Match != null) ? Match.GetPlayerGameScore(PlayerOrder.player1) : "0";
+            }
+        }
+        private int _player1WonGamesSet1;
+        public int Player1WonGamesSet1
+        {
+            get
+            {
+                return _player1WonGamesSet1;
+            }
+            set
+            {
+                _player1WonGamesSet1 = value;
+                OnPropertyChanged("Player1WonGamesSet1");
+            }
+        }
+        private int _player1WonGamesSet2;
+        public int Player1WonGamesSet2
+        {
+            get
+            {
+                return _player1WonGamesSet2;
+            }
+            set
+            {
+                _player1WonGamesSet2 = value;
+                OnPropertyChanged("Player1WonGamesSet2");
+            }
+        }
+        private int _player1WonGamesSet3;
+        public int Player1WonGamesSet3
+        {
+            get
+            {
+                return _player1WonGamesSet3;
+            }
+            set
+            {
+                _player1WonGamesSet3 = value;
+                OnPropertyChanged("Player1WonGamesSet3");
+            }
+        }
+        private int _player1WonGamesSet4;
+        public int Player1WonGamesSet4
+        {
+            get
+            {
+                return _player1WonGamesSet4;
+            }
+            set
+            {
+                _player1WonGamesSet4 = value;
+                OnPropertyChanged("Player1WonGamesSet4");
+            }
+        }
+        private int _player1WonGamesSet5;
+        public int Player1WonGamesSet5
+        {
+            get
+            {
+                return _player1WonGamesSet5;
+            }
+            set
+            {
+                _player1WonGamesSet5 = value;
+                OnPropertyChanged("Player1WonGamesSet5");
+            }
+        }
+        #endregion
+
+        #region Player2 properties
         private string _player2Name;
         public string Player2Name
         {
@@ -32,13 +113,6 @@ namespace TennisMatch.UI.Model
                 OnPropertyChanged("Player2Name");
             }
         }
-        public string Player1GameScore
-        {
-            get
-            {
-                return (Match != null) ? Match.GetPlayerGameScore(PlayerOrder.player1) : "0";
-            }
-        }
         public string Player2GameScore
         {
             get
@@ -46,6 +120,72 @@ namespace TennisMatch.UI.Model
                 return (Match != null) ? Match.GetPlayerGameScore(PlayerOrder.player2) : "0";
             }
         }
+        private int _player2WonGamesSet1;
+        public int Player2WonGamesSet1
+        {
+            get
+            {
+                return _player2WonGamesSet1;
+            }
+            set
+            {
+                _player2WonGamesSet1 = value;
+                OnPropertyChanged("Player2WonGamesSet1");
+            }
+        }
+        private int _player2WonGamesSet2;
+        public int Player2WonGamesSet2
+        {
+            get
+            {
+                return _player2WonGamesSet2;
+            }
+            set
+            {
+                _player2WonGamesSet2 = value;
+                OnPropertyChanged("Player2WonGamesSet2");
+            }
+        }
+        private int _player2WonGamesSet3;
+        public int Player2WonGamesSet3
+        {
+            get
+            {
+                return _player2WonGamesSet3;
+            }
+            set
+            {
+                _player2WonGamesSet3 = value;
+                OnPropertyChanged("Player2WonGamesSet3");
+            }
+        }
+        private int _player2WonGamesSet4;
+        public int Player2WonGamesSet4
+        {
+            get
+            {
+                return _player2WonGamesSet4;
+            }
+            set
+            {
+                _player2WonGamesSet4 = value;
+                OnPropertyChanged("Player2WonGamesSet4");
+            }
+        }
+        private int _player2WonGamesSet5;
+        public int Player2WonGamesSet5
+        {
+            get
+            {
+                return _player2WonGamesSet5;
+            }
+            set
+            {
+                _player2WonGamesSet5 = value;
+                OnPropertyChanged("Player2WonGamesSet5");
+            }
+        }
+        #endregion
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -78,7 +218,37 @@ namespace TennisMatch.UI.Model
 
                 OnPropertyChanged("Player1GameScore");
                 OnPropertyChanged("Player2GameScore");
+
+                UpdateWonGames(); // if needed, update the view with the won games
             }
+        }
+
+        private void UpdateWonGames()
+        {
+            if (Player1WonGamesSet1 != Match.GetPlayerSetScore(PlayerOrder.player1, 0))
+                Player1WonGamesSet1 = Match.GetPlayerSetScore(PlayerOrder.player1, 0);
+            if (Player2WonGamesSet1 != Match.GetPlayerSetScore(PlayerOrder.player2, 0))
+                Player2WonGamesSet1 = Match.GetPlayerSetScore(PlayerOrder.player2, 0);
+
+            if (Player1WonGamesSet2 != Match.GetPlayerSetScore(PlayerOrder.player1, 1))
+                Player1WonGamesSet2 = Match.GetPlayerSetScore(PlayerOrder.player1, 1);
+            if (Player2WonGamesSet2 != Match.GetPlayerSetScore(PlayerOrder.player2, 1))
+                Player2WonGamesSet2 = Match.GetPlayerSetScore(PlayerOrder.player2, 1);
+
+            if (Player1WonGamesSet3 != Match.GetPlayerSetScore(PlayerOrder.player1, 2))
+                Player1WonGamesSet3 = Match.GetPlayerSetScore(PlayerOrder.player1, 2);
+            if (Player2WonGamesSet3 != Match.GetPlayerSetScore(PlayerOrder.player2, 2))
+                Player2WonGamesSet3 = Match.GetPlayerSetScore(PlayerOrder.player2, 2);
+
+            if (Player1WonGamesSet4 != Match.GetPlayerSetScore(PlayerOrder.player1, 3))
+                Player1WonGamesSet4 = Match.GetPlayerSetScore(PlayerOrder.player1, 3);
+            if (Player2WonGamesSet4 != Match.GetPlayerSetScore(PlayerOrder.player2, 3))
+                Player2WonGamesSet4 = Match.GetPlayerSetScore(PlayerOrder.player2, 3);
+
+            if (Player1WonGamesSet5 != Match.GetPlayerSetScore(PlayerOrder.player1, 4))
+                Player1WonGamesSet5 = Match.GetPlayerSetScore(PlayerOrder.player1, 4);
+            if (Player2WonGamesSet5 != Match.GetPlayerSetScore(PlayerOrder.player2, 4))
+                Player2WonGamesSet5 = Match.GetPlayerSetScore(PlayerOrder.player2, 4);
         }
     }
 }
